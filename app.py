ba9d2a0db38058a2c3e8affe5922ce815d87cccb
JONATHAN DABREO <68890908+Obnoxious-coder@users.noreply.github.com>
2020-12-11 10:05:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request
+
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+        print("file recieved")
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        
+    return render_template('home.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ convert.py
@@ -0,0 +1,27 @@
+def text_to_binary(text):
+    print("The original string is : " + str(text)) 
+    text=text.replace(" ", "")
+    res = ''.join(format(ord(x), 'b') for x in text)
+    print(len(text))
+    return res
+
+def BinaryToDecimal(binary):   
+    decimal, i = 0, 0
+    while(binary != 0):  
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)  
+        binary = binary//10
+        i += 1
+    return (decimal)
+
+
+def binary_to_text(text):
+    print("The binary value is:", text) 
+    str_data =''
+    for i in range(0, len(text), 7): 
+        temp_data = int(text[i:i + 7]) 
+        decimal_data = BinaryToDecimal(temp_data) 
+        str_data = str_data + chr(decimal_data) 
+    print(str_data) 
+    return str_data
+   
